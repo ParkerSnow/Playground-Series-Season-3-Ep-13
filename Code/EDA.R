086502e8ccc45fc88f9472c5fc1c6f6810b5f83b
ParkerSnow <parkersnow40@yahoo.com>
2023-04-18 12:15:34 -0600

    Added a way to view some of the data

--- Code/EDA.R
+++ Code/EDA.R
@@ -4,4 +4,11 @@ library(tidyverse)
 
 train <- read.csv('train.csv')
 
+train$prognosis <- as_factor(train$prognosis)
+
 summary(train)
+
+train %>% gather()
+ggplot(gather(train),aes(value,stat="count")) + geom_bar() + facet_wrap(~key,scales='free_x')
+
+ggplot(train,aes(x=prognosis,stat='count',group = abdominal_pain)) + geom_bar(aes(fill=abdominal_pain),position = 'stack')
