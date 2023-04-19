b425821035b1371e5cd0fb9522506b13205cd3a4
ParkerSnow <parkersnow40@yahoo.com>
2023-04-19 11:33:24 -0600

    Added ways to create models and look at data

--- Code/ModelGeneration.py
+++ Code/ModelGeneration.py
@@ -4,3 +4,25 @@ from sklearn import tree, gaussian_process, ensemble, feature_selection, pipelin
 from sklearn.base import TransformerMixin, BaseEstimator
 import pandas as pd
 
+data = pd.read_csv('Data/train.csv')
+#trainTarget = train.pop('prognosis')
+#print(train.head())
+#print(trainTarget.head())
+#print(trainTarget.shape)
+
+trainX, testX = model_selection.train_test_split(data,random_state=1,train_size=0.7,shuffle=True)
+
+trainY = trainX.pop('prognosis')
+testY = testX.pop('prognosis')
+
+print(trainX.head())
+print(trainY.head())
+
+model = sk.linear_model.LogisticRegression()
+model.fit(trainX[['sudden_fever','headache']],trainY)
+print(model.classes_)
+trainPredictions = model.predict_proba(trainX[['sudden_fever','headache']])
+testPredictions = model.predict_proba(testX[['sudden_fever','headache']])
+
+print(trainPredictions)
+print(testPredictions)
