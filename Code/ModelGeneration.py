c22bd40e6101b0d620fef27e6bffb1988bb9e605
ParkerSnow <parkersnow40@yahoo.com>
2023-04-19 10:59:35 -0600

    Add python file

---
+++ Code/ModelGeneration.py
@@ -0,0 +1,6 @@
+import numpy as np
+import sklearn as sk
+from sklearn import tree, gaussian_process, ensemble, feature_selection, pipeline, model_selection
+from sklearn.base import TransformerMixin, BaseEstimator
+import pandas as pd
+
